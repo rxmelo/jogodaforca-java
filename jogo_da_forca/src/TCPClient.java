55a653f2c1e8924010542e395f2803c61f26e9b3
rxmelo <rxm@discente.ifpe.edu.br>
2026-01-21 18:11:50 -0300

    Update TCPClient.java

--- jogo_da_forca/src/TCPClient.java
+++ jogo_da_forca/src/TCPClient.java
@@ -5,43 +5,37 @@ import java.net.Socket;
 
 public class TCPClient {
 
-	public static void main(String argv[]) throws Exception {
-		
-		// informações de porta e ip
-		String serverAddress = "localhost";
-		int serverPort = 6789;
-		// criação do socket que irá conectar o cliente ao servidor
-		Socket clientSocket = new Socket(serverAddress, serverPort);
-		
-		System.out.println(" Cliente conectado ao TCP server " + serverAddress+":"+serverPort);
-		// bloco primordial para a comunicação e leitura de dados
-		BufferedReader ler = new BufferedReader(new InputStreamReader(System.in)); // leitor do teclado do cliente
-		DataOutputStream sai = new DataOutputStream(clientSocket.getOutputStream()); //output
-		BufferedReader entra = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //input
-		
-        // primeira mensagem do servidor
-        String mensagem_inicial = entra.readLine();
-        System.out.println(mensagem_inicial);
-        //regra 1
-        System.out.println(entra.readLine());
-        //regra 2
-        System.out.println(entra.readLine());
-        //regra 3
-        System.out.println(entra.readLine());
-        // loop do jogo
-        while (true) {
+    public static void main(String argv[]) throws Exception {
 
+        Socket clientSocket = new Socket("localhost", 6789);
+        System.out.println("Cliente conectado ao servidor");
+
+        BufferedReader ler = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream sai = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader entra = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        while (true) {
+        	// recebimento de mensages do servidor
             String mensagem = entra.readLine();
-            // servidor encerrou
+            // caso o servidor feche
             if (mensagem == null) break;
             System.out.println(mensagem);
-            // pedido do servidor
+            // comfirmação
+            if (mensagem.toLowerCase().contains("confirmar")) {
+                String resp = ler.readLine();
+                sai.writeBytes(resp + "\n");
+            }
+            // controle de turno do jogador
             if (mensagem.contains("Digite uma letra")) {
                 String palpite = ler.readLine();
                 sai.writeBytes(palpite + "\n");
             }
+            // controle de nova rodada
+            if (mensagem.contains("novo jogo?")) {
+            	String palpite = ler.readLine();
+                sai.writeBytes(palpite + "\n");
+            }
         }
-	
 
         clientSocket.close();
         System.out.println("Conexão encerrada.");
